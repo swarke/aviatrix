cea74caff4b8ed1b3ec4961b55cd6246829fb200
Sachin Warke <sachin.warke@opcito.com>
2017-04-25 02:46:15 +0530

    Added charts and calculated statistics.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { ChartModule } from 'angular2-highcharts';
+import { DashboardModel} from '../../models';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +9,7 @@ import { ChartModule } from 'angular2-highcharts';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-	clouds: any;
+  clouds: any;
   options: any;
   latencyOptions: any;
   responseTimeOptions: any;
@@ -17,7 +19,23 @@ export class DashboardComponent implements OnInit {
   lng: number;
 
   inventory: any;
-  constructor() {
+
+  dashboardModel: DashboardModel;
+  pingStartTime: any =  null;
+
+  TEST_MINUTES: number = 1;
+
+  latency: any = 'NA';
+  bandwidth: any = 'NA';
+  responseTime: any = 'NA';
+  throughput: any = 'NA';
+
+  latencyChart: any;
+
+  chart : Object;
+
+  constructor(private _http: Http) {
+      this.dashboardModel = new DashboardModel();
   	  this.clouds = [
   	    {value: '0', viewValue: 'All Cloud'},
   	    {value: '1', viewValue: 'Google Cloud'},
@@ -26,7 +44,7 @@ export class DashboardComponent implements OnInit {
      this.lat = 37.646152;
      this.lng = -77.511429;
      this.options = [];
-     this.renderAllCharts();
+     // this.renderAllCharts();
      this.inventory = {
         'aws': [{
           'name': 'aviatrix-us-east-1-test1',
@@ -37,13 +55,17 @@ export class DashboardComponent implements OnInit {
             'region': 'us-east-1',
             'availability_zone': 'us-east-1c',
           },
-          'url': 'url',
+          'url': 'http://107.22.156.82/',
           'lat': '41.283142',
           'lng': '-72.872870',
         }]
       };
   }
 
+  saveInstance(chartInstance) {
+    this.chart = chartInstance;
+  }
+  
   ngOnInit() {
     //this.renderAllCharts();
   }
@@ -92,6 +114,17 @@ export class DashboardComponent implements OnInit {
   getChartConfig (title: any, unit: any, series: any, chartType: any) {
      const options = {
           chart:   { type:  chartType, zoomType:   'xy' },
+          // events: {
+          //       load: function () {
+          //           // set up the updating of the chart each second
+          //           var series = this.series[0];
+          //           setInterval(function () {
+          //               var x = new Date(), // current time
+          //                   y = Math.random();
+          //               series.addPoint([x, y], true, true);
+          //           }, 1000);
+          //       }
+          // }, 
           title :   { text :   title },
           global :   {
             useUTC :   false,
@@ -104,7 +137,7 @@ export class DashboardComponent implements OnInit {
                     format: '{value}'
                   },
                   title:   {
-                  text: unit
+                    text: unit
                   }
           },
           series: series
@@ -113,31 +146,31 @@ export class DashboardComponent implements OnInit {
     }
 
    generateChart (chartFor: any) { 
-     if(chartFor === 'latency'){
-       let series: any = [];
-         series.push(this.getSeriesData('spline', "us-east-1", this.getChartData(this.getLatencyData())));
-         series.push(this.getSeriesData('spline', "us-west-2", this.getChartData(this.getLatencyData2())));
-         const chartConfig = this.getChartConfig('', '', series, 'spline');
-         this.latencyOptions = chartConfig;    
-     } else if (chartFor === 'responseTime'){
-         let series: any = [];
-         series.push(this.getSeriesData('spline', "us-east-1", this.getChartData(this.getResponsetime1())));
-         series.push(this.getSeriesData('spline', "us-west-2", this.getChartData(this.getResponsetime2())));
-         const chartConfig = this.getChartConfig('', '', series, 'spline');
-         this.responseTimeOptions = chartConfig; 
-     } else if (chartFor === 'packetLoss'){
-         let series: any = [];
-         series.push(this.getSeriesData('column', "us-east-1", this.getChartData(this.getPacketLossData())));
-         series.push(this.getSeriesData('column', "us-west-2", this.getChartData(this.getPacketLossData2())));
-         const chartConfig = this.getChartConfig('', '', series, 'column');
-         this.packetLossOptions = chartConfig; 
-     } else if (chartFor === 'throughput'){
-       let series: any = [];
-         series.push(this.getSeriesData('column', "us-east-1", this.getChartData(this.getThroughputData())));
-         series.push(this.getSeriesData('column', "us-west-2", this.getChartData(this.getThroughputData2())));
-         const chartConfig = this.getChartConfig('', '', series, 'column');
-         this.throughputOptions = chartConfig; 
-     }
+     // if(chartFor === 'latency') {
+     //   let series: any = [];
+     //     series.push(this.getSeriesData('spline', "us-east-1", this.getChartData(this.getLatencyData())));
+     //     // series.push(this.getSeriesData('spline', "us-west-2", this.getChartData(this.getLatencyData2())));
+     //     const chartConfig = this.getChartConfig('', '', series, 'spline');
+     //     this.latencyOptions = chartConfig;    
+     // } else if (chartFor === 'responseTime') {
+     //     let series: any = [];
+     //     series.push(this.getSeriesData('spline', "us-east-1", this.getChartData(this.getResponsetime1())));
+     //     series.push(this.getSeriesData('spline', "us-west-2", this.getChartData(this.getResponsetime2())));
+     //     const chartConfig = this.getChartConfig('', '', series, 'spline');
+     //     this.responseTimeOptions = chartConfig; 
+     // } else if (chartFor === 'packetLoss') {
+     //     let series: any = [];
+     //     series.push(this.getSeriesData('column', "us-east-1", this.getChartData(this.getPacketLossData())));
+     //     series.push(this.getSeriesData('column', "us-west-2", this.getChartData(this.getPacketLossData2())));
+     //     const chartConfig = this.getChartConfig('', '', series, 'column');
+     //     this.packetLossOptions = chartConfig; 
+     // } else if (chartFor === 'throughput') {
+     //   let series: any = [];
+     //     series.push(this.getSeriesData('column', "us-east-1", this.getChartData(this.getThroughputData())));
+     //     series.push(this.getSeriesData('column', "us-west-2", this.getChartData(this.getThroughputData2())));
+     //     const chartConfig = this.getChartConfig('', '', series, 'column');
+     //     this.throughputOptions = chartConfig; 
+     // }
 
   }
 
@@ -501,7 +534,7 @@ export class DashboardComponent implements OnInit {
       const jsonObj = chartData[index];
       if (jsonObj.value !== null) {
         const date: Date = new Date(jsonObj.time);
-        let yVal = jsonObj.value; 
+        let yVal = jsonObj.value;
         
         metricData.push([Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(),
          date.getHours(), date.getMinutes(), date.getSeconds()), yVal]);
@@ -511,6 +544,198 @@ export class DashboardComponent implements OnInit {
     return metricData;
   }
 
+  /**
+   * Starts test for calculating the statistics.
+   */
+  startTest() {
+    this.pingStartTime = new Date();
+    this.latency =  'NA';
+    this.responseTime = 'NA';
+    this.bandwidth = 'NA';
+    this.setLatency();
+    this.setBandwith();
+    this.setResponseTime();
+   
+    // setInterval(() => this.latencyOptions.series[0].addPoint(Math.random() * 10), 1000);
+     // let series: any = [];
+     // series.push(this.getSeriesData('spline', "us-east-1", this.getChartData(this.dashboardModel.latency)));
+     // this.latencyOptions = this.getChartConfig('', 'Miliseconds', series, 'spline');
+  }
+
+  getTimeDiff() {
+    let endTime:any = new Date();
+    let diff: any = endTime - this.pingStartTime;
+
+    var diffMins = Math.round(((diff % 86400000) % 3600000) / 60000);
+    console.log('Diff is ' + diffMins);
+
+    return diffMins;
+  }
+
+  getTimeDiffInSeconds() {
+    let endTime:any = new Date();
+    let diff: any = endTime.getTime() - this.pingStartTime.getTime();
+
+    var diffSec = diff/ 1000;
+    console.log('Diff is diffSec' + diffSec);
+
+    return diffSec;
+  }
+
+  setBandwith() {
+    console.log("In bandwidth");
+    var downloadSize = 2621440; //bytes
+    let dashboard = this;
+    if (this.getTimeDiff() < this.TEST_MINUTES) {
+        let pingStart = performance.now();
+        var cacheBuster = "?nnn=" + pingStart;
+        this._http.get(this.inventory.aws[0].url + 'clouds-01.jpeg' + cacheBuster)
+          .subscribe((data) => {
+            // console.log("data : " + data);
+            let pingEnd: number = performance.now();
+            let duration: number = ((pingEnd - pingStart)/1000);
+            console.log("Duration Second: " + duration);
+            let bitsLoaded = downloadSize * 8;
+            let speedBps: any = (bitsLoaded / duration).toFixed(2);
+            let speedKbps: any = (speedBps / 1024).toFixed(2);
+            let speedMbps = (speedKbps / 1024).toFixed(2);
+
+            dashboard.dashboardModel.bandwidth.push({'time': pingEnd, 'value': speedMbps});
+              // dashboard.setThroughput(bitsLoaded,pingEnd, duration);
+            dashboard.getBandwidth();
+              // dashboard.getThroughput();
+            dashboard.setBandwith();
+        });
+          // var download = new Image();
+          // download.onload = function () {
+          //     let pingEnd: number = performance.now();
+          //     let duration: number = ((pingEnd - pingStart)/1000);
+          //     let bitsLoaded = downloadSize * 8;
+          //     let speedBps: any = (bitsLoaded / duration).toFixed(2);
+          //     let speedKbps: any = (speedBps / 1024).toFixed(2);
+          //     let speedMbps = (speedKbps / 1024).toFixed(2);
+        
+          //     dashboard.dashboardModel.bandwidth.push({'time': pingEnd, 'value': speedMbps});
+          //     dashboard.setThroughput(bitsLoaded,pingEnd, duration);
+          //     dashboard.getBandwidth();
+          //     dashboard.getThroughput();
+          //     dashboard.setBandwith();
+          // } 
+
+          // let pingStart = performance.now();
+          // var cacheBuster = "?nnn=" + pingStart;
+          // download.src = this.inventory.aws[0].url + 'clouds-01.jpeg' + cacheBuster;
+      }
+  }
+
+  getBandwidth() {
+    if (this.dashboardModel.bandwidth.length > 0) {
+      let _bandwidth:number = 0;
+      for (let index = 0 ; index < this.dashboardModel.bandwidth.length; index++) {
+        _bandwidth = _bandwidth + parseFloat(this.dashboardModel.bandwidth[index].value);
+      }
+     this.bandwidth =  _bandwidth / this.dashboardModel.bandwidth.length;
+    }
+  }
+
+  setLatency() {
+    console.log("In Latency");
+    if (this.getTimeDiff() < this.TEST_MINUTES) {
+       let pingStart = performance.now();
+        this._http.get(this.inventory.aws[0].url)
+          .subscribe((data) => {
+            let pingEnd: number = performance.now();
+            let ping: number = (pingEnd - pingStart);
+            this.dashboardModel.latency.push({'time': new Date(), 'value': Math.round(ping)});
+
+            let series: any = [];
+            this.latencyOptions = [];
+            series.push(this.getSeriesData('spline', "us-east-1", this.getChartData(this.dashboardModel.latency)));
+            this.latencyOptions = this.getChartConfig('', 'Miliseconds', series, 'spline');
+            
+
+            this.getLatency();
+            this.setLatency();
+        });
+    } else {
+      let series: any = [];
+      this.latencyOptions = [];
+      series.push(this.getSeriesData('spline', "us-east-1", this.getChartData(this.dashboardModel.latency)));
+      this.latencyOptions = this.getChartConfig('', 'Miliseconds', series, 'spline');
+    }
+  }
+
+  getLatency() {
+    if (this.dashboardModel.latency.length > 0) {
+      let _latency:number = 0;
+      for (let index = 0 ; index < this.dashboardModel.latency.length; index++) {
+        _latency = _latency + parseFloat(this.dashboardModel.latency[index].value);
+      }
+
+     this.latency =  _latency / this.dashboardModel.latency.length;
+    }
+  }
+
+  setResponseTime() {
+    console.log("In Response Time");
+    if (this.getTimeDiff() < this.TEST_MINUTES) {
+       let pingStart = performance.now();
+        this._http.get(this.inventory.aws[0].url + 'test.html')
+          .subscribe((data) => {
+            let pingEnd: number = performance.now();
+            let ping: number = pingEnd - pingStart;
+            this.dashboardModel.responseTime.push({'time': new Date(), 'value': Math.round(ping)});
+
+            let series: any = [];
+            this.responseTimeOptions = [];
+            series.push(this.getSeriesData('spline', "us-east-1", this.getChartData(this.dashboardModel.responseTime)));
+            this.responseTimeOptions = this.getChartConfig('', 'Miliseconds', series, 'spline');
+
+            this.getResponseTime();
+            this.setResponseTime();
+        });
+    } else {
+        let series: any = [];
+        this.responseTimeOptions = [];
+        series.push(this.getSeriesData('spline', "us-east-1", this.getChartData(this.dashboardModel.responseTime)));
+        this.responseTimeOptions = this.getChartConfig('', 'Miliseconds', series, 'spline');
+    }
+  }
+
+  getResponseTime() {
+    if (this.dashboardModel.responseTime.length > 0) {
+      let _responseTime:number = 0;
+      for (let index = 0 ; index < this.dashboardModel.responseTime.length; index++) {
+        _responseTime = _responseTime + parseFloat(this.dashboardModel.responseTime[index].value);
+      }
+
+     this.responseTime =  _responseTime / this.dashboardModel.responseTime.length;
+    }
+  }
+
+  setThroughput(bandwidth, time, duration) {
+    var seconds = ((time % 60000) / 1000)
+    console.log("seconds ==>> " + seconds + " throuput ==>> " + (bandwidth / seconds));
+    var _bandwidth = (bandwidth / seconds);
+    let speedBps: any = (_bandwidth / duration).toFixed(2);
+    let speedKbps: any = (speedBps / 1024).toFixed(2);
+    let speedMbps = (speedKbps / 1024).toFixed(2);
+    this.dashboardModel.throughput.push({'time': time, 'value': speedMbps});
+  }
+
+  getThroughput() {
+    if (this.dashboardModel.throughput.length > 0) {
+      let _throughput:number = 0;
+      for (let index = 0 ; index < this.dashboardModel.throughput.length; index++) {
+        _throughput = _throughput + parseFloat(this.dashboardModel.throughput[index].value);
+      }
+
+     console.log("before Throughput " + _throughput);
+ 
+     this.throughput =  _throughput / this.dashboardModel.throughput.length;
+     console.log("After Throughput " + _throughput);
+    }
+  }
 }
 
 // just an interface for type safety.
