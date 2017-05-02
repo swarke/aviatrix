eef2e7349ca37bd0e88852c4deb0326224906080
Sachin Warke <sachin.warke@opcito.com>
2017-05-02 17:54:52 +0530

    Applied map styles.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -51,6 +51,8 @@ export class DashboardComponent implements OnInit {
 
   bestRegion: any;
   worstRegion: any;
+
+  mapStyles: any;
       
   constructor(private mapsAPILoader : MapsAPILoader,
               private http: Http,
@@ -76,6 +78,188 @@ export class DashboardComponent implements OnInit {
      this.selectedRegions = [];
      this.bestRegion = null;
      this.worstRegion = null;
+     this.mapStyles = [
+                          {
+                              "elementType": "geometry",
+                              "stylers": [{
+                                "color": "#f5f5f5"
+                              }]
+                          },
+                          {
+                            "elementType": "labels",
+                            "stylers": [
+                              {
+                                "visibility": "off"
+                              }
+                            ]
+                          },
+                          {
+                            "elementType": "labels.icon",
+                            "stylers": [
+                              {
+                                "visibility": "off"
+                              }
+                            ]
+                          },
+                          {
+                            "elementType": "labels.text.fill",
+                            "stylers": [
+                              {
+                                "color": "#616161"
+                              }
+                            ]
+                          },
+                          {
+                            "elementType": "labels.text.stroke",
+                            "stylers": [
+                              {
+                                "color": "#f5f5f5"
+                              }
+                            ]
+                          },
+                          {
+                            "featureType": "administrative",
+                            "elementType": "geometry",
+                            "stylers": [{
+                              "visibility": "off"
+                            }]
+                          },
+                          {
+                            "featureType": "administrative.land_parcel",
+                            "stylers": [{
+                              "visibility": "off"
+                            }]
+                          },
+                          {
+                            "featureType": "administrative.land_parcel",
+                            "elementType": "labels.text.fill",
+                            "stylers": [{
+                              "color": "#bdbdbd"
+                            }]
+                          },
+                          {
+                            "featureType": "administrative.neighborhood",
+                            "stylers": [{
+                              "visibility": "off"
+                            }]
+                          },
+                          {
+                            "featureType": "road",
+                            "stylers": [{
+                              "visibility": "off"
+                            }]
+                          },
+                          {
+                            "featureType": "road",
+                            "elementType": "geometry",
+                            "stylers": [{
+                              "color": "#ffffff"
+                            }]
+                          },
+                          {
+                            "featureType": "road",
+                            "elementType": "labels.icon",
+                            "stylers": [{
+                              "visibility": "off"
+                            }]
+                          },
+                          {
+                            "featureType": "road.arterial",
+                            "elementType": "labels.text.fill",
+                            "stylers": [{
+                              "color": "#757575"
+                            }]
+                          },
+                          {
+                            "featureType": "road.highway",
+                            "elementType": "geometry",
+                            "stylers": [{
+                              "color": "#dadada"
+                            }]
+                          },
+                          {
+                            "featureType": "road.highway",
+                            "elementType": "labels.text.fill",
+                            "stylers": [{
+                              "color": "#616161"
+                            }]
+                          },
+                          {
+                            "featureType": "road.local",
+                            "elementType": "labels.text.fill",
+                            "stylers": [{
+                              "color": "#9e9e9e"
+                            }]
+                            },
+                            {
+                              "featureType": "transit",
+                              "stylers": [{
+                                "visibility": "off"
+                              }]
+                            },
+                            {
+                              "featureType": "transit.line",
+                              "elementType": "geometry",
+                              "stylers": [{
+                                "color": "#e5e5e5"
+                              }]
+                            },
+                            {
+                              "featureType": "transit.station",
+                              "elementType": "geometry",
+                              "stylers": [{
+                                "color": "#eeeeee"
+                              }]
+                            },
+                            {
+                              "featureType": "water",
+                              "elementType": "geometry",
+                              "stylers": [{
+                                "color": "#c9c9c9"
+                              }]
+                            },
+                            {
+                              "featureType": "water",
+                              "elementType": "labels.text.fill",
+                              "stylers": [{
+                                "color": "#9e9e9e"
+                              }]
+                            },
+                            {
+                              "featureType": "poi",
+                              "stylers": [{
+                                "visibility": "off"
+                              }]
+                            },
+                            {
+                              "featureType": "poi",
+                              "elementType": "geometry",
+                              "stylers": [{
+                                "color": "#eeeeee"
+                              }]
+                            },
+                            {
+                              "featureType": "poi",
+                              "elementType": "labels.text.fill",
+                              "stylers": [{
+                                "color": "#757575"
+                              }]
+                            },
+                            {
+                              "featureType": "poi.park",
+                              "elementType": "geometry",
+                              "stylers": [{
+                                "color": "#e5e5e5"
+                              }]
+                            },
+                            {
+                              "featureType": "poi.park",
+                              "elementType": "labels.text.fill",
+                              "stylers": [{
+                                "color": "#9e9e9e"
+                              }]
+                            }];
+
   }
 
   latencyInstance(chartInstance) {
