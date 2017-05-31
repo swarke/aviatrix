7a9ed11a0e1b5b0fd3224e1e2ef5d1e0f754cde9
Sachin Warke <sachin.warke@opcito.com>
2017-05-31 23:52:46 +0530

    Fixed conflicts and improve performance of the map and chart.

--- src/services/dashboard.service.ts
+++ src/services/dashboard.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Inject } from '@angular/core';
 import { Response } from '@angular/http';
 import { APIRequest, APIUrls } from '../models';
 import { APIMethod, ApiService } from './api.service';
+import { PropertiesService } from './properties.service';
 import { Observable } from 'rxjs/Observable';
 
 declare const AWS: any;
@@ -14,7 +15,8 @@ declare const AWS: any;
  */
 export class DashboardService {
 
-  constructor(private _apiService: ApiService) {
+  constructor(private _apiService: ApiService,
+              private properties: PropertiesService) {
     //this.createAWS();
     
   }
@@ -26,8 +28,8 @@ export class DashboardService {
   }
 
 
-getGeolocation() {
-   const apiRequest: APIRequest = new APIRequest('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDCa1LUe1vOczX1hO_iGYgyo8p_jYuGOPU', APIMethod.POST);
+  getGeolocation() { 
+   const apiRequest: APIRequest = new APIRequest('https://www.googleapis.com/geolocation/v1/geolocate?key=' + this.properties.GOOGLE_API_KEY, APIMethod.POST);
     return this._apiService.executeAPI(apiRequest);
    // return this.http.post(, {});
   };
@@ -45,47 +47,6 @@ getGeolocation() {
   getBandwidth(url) {
     const apiRequest: APIRequest = new APIRequest(url, APIMethod.GET);
     return this._apiService.executeAPI(apiRequest);
-  }
-
-  createAWS() {
-    AWS.config.credentials  = new AWS.Credentials({
-      accessKeyId: 'AKIAJJJILAZL5ALJMD3A', secretAccessKey: '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'
-    });
-
-    AWS.config.region = 'us-west-2';
-
-    var ses = new AWS.SES();
-
-     var params = {
-      Source:'sachin.warke@opcito.com',
-      Destination:{
-          'ToAddresses': [
-              'chaitanya.deshpande@opcito.com',
-          ],
-      },
-      Message:{
-          'Subject': {
-              'Data': 'Test Email',
-          },
-          'Body': {
-              'Html': {
-                  'Data': '<b> Success</b>',
-              },
-          }
-      }
-  
-     };
- ses.sendEmail(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-   /*
-   data = {
-    MessageId: "EXAMPLE78603177f-7a5433e7-8edb-42ae-af10-f0181f34d6ee-000000"
-   }
-   */
- });
-
-    // console.log("Creds: " + JSON.stringify(creds));
   }
 
 }
