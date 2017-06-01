45e103d9a27fcdfb367a44edf18d20e724f14dd7
Sachin Warke <sachin.warke@opcito.com>
2017-06-02 02:34:59 +0530

    Commented landing page routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AWSComponent } from './aws/aws.component';
 import { AzureComponent } from './azure/azure.component';
 
 const AppRoutes: Routes = [
-	{ path: '', component: LandingComponent },
+	// { path: '', component: LandingComponent },
 	{ path: 'aws/speedtest', component: AWSComponent },
 	{ path: 'azure/speedtest', component: AzureComponent},
 ];
